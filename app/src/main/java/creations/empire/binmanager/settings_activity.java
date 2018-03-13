1b8f9959237a833a7b50466dea39ea8384547b85
Lin Sanjo <linsanjo7@gmail.com>
2018-03-13 10:54:43 +0530

    updated

--- app/src/main/java/creations/empire/binmanager/settings_activity.java
+++ app/src/main/java/creations/empire/binmanager/settings_activity.java
@@ -1,16 +1,31 @@
 package creations.empire.binmanager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-/**
- * Created by user on 13-03-2018.
- */
 
 public class settings_activity extends AppCompatActivity{
+    private Button button;
+    EditText ipaddress,portnumber;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting_layout);
+        button = findViewById(R.id.button3);
+        ipaddress = findViewById(R.id.editText3);
+        portnumber = findViewById(R.id.editText4);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(settings_activity.this, LoginActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
