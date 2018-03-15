f757584f553b158c55c82f0295e71c817acf08ce
Lin Sanjo <linsanjo7@gmail.com>
2018-03-16 02:59:29 +0530

    updated

--- app/src/main/java/creations/empire/binmanager/MainActivity.java
+++ app/src/main/java/creations/empire/binmanager/MainActivity.java
@@ -1,23 +1,40 @@
 package creations.empire.binmanager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    SharedPreferences sp;
+    SharedPreferences.Editor ed;
+    TextView name,place,number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button view = findViewById(R.id.button2);
+        sp = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        ed = sp.edit();
+        name = findViewById(R.id.textView);
+        place = findViewById(R.id.textView2);
+        number = findViewById(R.id.textView4);
+        name.setText(sp.getString("name","Driver Name"));
+        place.setText(sp.getString("place","Driver place"));
+        number.setText(sp.getString("number","Driver number"));
+        Toast.makeText(this, ""+sp.getString("email_id","null"), Toast.LENGTH_SHORT).show();
         view.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(i);
+
             }
         });
     }
--- app/src/main/java/creations/empire/binmanager/settings_activity.java
+++ app/src/main/java/creations/empire/binmanager/settings_activity.java
@@ -1,12 +1,15 @@
 package creations.empire.binmanager;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class settings_activity extends AppCompatActivity{
@@ -20,11 +23,26 @@ public class settings_activity extends AppCompatActivity{
         button = findViewById(R.id.button3);
         ipaddress = findViewById(R.id.editText3);
         portnumber = findViewById(R.id.editText4);
+        final ConnectivityManager connectivityManager = ((ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE));
+        if(connectivityManager.getActiveNetworkInfo().isConnected())
+        {
+            Toast.makeText(this, "Connection Active", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "No Connection", Toast.LENGTH_SHORT).show();
+        }
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(settings_activity.this, LoginActivity.class);
-                startActivity(i);
+                if(ipaddress.getText().toString().equals("") || portnumber.getText().toString().equals("") )
+                    Toast.makeText(settings_activity.this, "Enter IP and Port Number", Toast.LENGTH_SHORT).show();
+                else {
+                    i.putExtra("ip", ipaddress.getText().toString());
+                    i.putExtra("port", portnumber.getText().toString());
+                    startActivity(i);
+                }
             }
         });
     }
